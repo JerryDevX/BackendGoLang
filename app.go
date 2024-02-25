cb9923b452a6abbcddf0cf5c87ed415730d35727
JerryDevTeam <155259170+JerryDevX@users.noreply.github.com>
2024-02-26 03:06:51 +0700

    x

--- app.go
+++ app.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v3"
 )
 
@@ -9,9 +11,13 @@ func main() {
 
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
+	app.Get("/", func(c fiber.Ctx) error {
+        return c.SendString("TEST") // => Hello john 👋!
+    })
 
-	app.listen(":3000")
+    log.Fatal(app.Listen(":3000"))
+
+
+	app.Listen(":3000")
+
 }
